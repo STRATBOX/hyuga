74f0b1953972afc9c6e649c33ece117ceec066e4
Pepeye <gaofolaju@gmail.com>
2020-04-04 00:56:02 +0100

    feat: initial push
    - adds actix web server
    - implements custom responder
    - adds chrono and serde packages

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use actix_web::{web, middleware, App, Error, HttpRequest, HttpResponse, HttpServer,  Responder};
+use chrono::Utc;
+use futures::future::{ready, Ready};
+use serde::{Serialize};
+use ulid::Ulid;
+
+struct AppState {
+    app_name: &'static str
+}
+
+#[derive(Serialize)]
+struct Ping {
+    id: String,
+    service: String,
+    message: String,
+    created_on: i64
+}
+
+impl Responder for Ping {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)
+        ))
+    }
+}
+
+async fn index(data: web::Data<AppState>) -> impl Responder {
+    let p: Ping = Ping {
+        id: Ulid::new().to_string().to_lowercase(),
+        service: data.app_name.to_string(),
+        message: String::from("running..."),
+        created_on: Utc::now().timestamp_nanos()
+    };
+    p
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {}!", name)
+}
+
+#[actix_rt::main]   
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .data(AppState {
+                app_name: "hyuga"
+            })
+            .route("/", web::get().to(index))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:5000")?
+    .run()
+    .await
+}
